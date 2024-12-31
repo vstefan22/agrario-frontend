c47544177b1e890339d2132ff0e774b2a2359cd9
aleksasavic1 <asavic846@gmail.com>
2024-12-31 15:16:41 +0100

    Add Thank You Marketing Page

--- src/types/table-data-types.tsx
+++ src/types/table-data-types.tsx
@@ -20,3 +20,14 @@ export const WARENKORB_ANALYSE_PLUS_COLUMNS = [
   { key: 'nll', label: 'Nutzung laut Liegenschaftskataster' },
   { key: 'grobe', label: 'Größe' },
 ]
+
+export const THANK_YOU_MARKETING_COLUMNS = [
+  { key: 'state', label: 'Bundesland' },
+  { key: 'plz', label: 'PLZ' },
+  { key: 'city', label: 'Gemeinde' },
+  { key: 'district', label: 'Gemarkung' },
+  { key: 'flur', label: 'Flur' },
+  { key: 'flurstuck', label: 'Flurstuck' },
+  { key: 'registry', label: 'Nutzung laut Liegenschaftskataster' },
+  { key: 'size', label: 'Größe' },
+];
