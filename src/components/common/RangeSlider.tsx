50ee33c91abc5188f5563542bcbcfdbc18eddcf8
aleksasavic1 <asavic846@gmail.com>
2025-01-04 11:41:35 +0100

    Add RangeSlider functionality

--- src/components/common/RangeSlider.tsx
+++ src/components/common/RangeSlider.tsx
@@ -4,24 +4,40 @@ import { useState } from 'react';
 interface rangeSliderTypes {
   title?: string;
   details?: string;
+  unit?: string;
+  onFilter: (range: [number, number]) => void;
 }
 
-const RangeSlider = ({ title, details }: rangeSliderTypes) => {
-  const [values, setValues] = useState([0.2, 200]);
+const RangeSlider = ({ title, details, onFilter, unit }: rangeSliderTypes) => {
+  // Srediti podatke i funkcije da budu reusable kada se bude radio r2v4
+  const [values, setValues] = useState<[number, number]>([0.2, 200]);
   const STEP = 0.1;
   const MIN = 0;
   const MAX = 250;
 
+  const handleFinalChange = (finalValues: [number, number]) => {
+    const newRange: [number, number] = [
+      finalValues[0] * 100,
+      finalValues[1] * 100,
+    ];
+    onFilter(newRange);
+  };
+
   return (
     <div className='w-full h-[100px] px-4 text-[15px] flex flex-col rounded-[6px] mt-4'>
-      <p className='text-[14px] text-black-muted mb-4 font-bold'>{title}</p>
+      {title && (
+        <p className='text-[14px] text-black-muted mb-4 font-bold'>{title}</p>
+      )}
       <div className='relative mt-4'>
         <Range
           step={STEP}
           min={MIN}
           max={MAX}
           values={values}
-          onChange={(values) => setValues(values)}
+          onChange={(newValues) => setValues(newValues as [number, number])}
+          onFinalChange={(finalValues) =>
+            handleFinalChange(finalValues as [number, number])
+          }
           renderTrack={({ props, children }) => (
             <div
               {...props}
@@ -41,41 +57,45 @@ const RangeSlider = ({ title, details }: rangeSliderTypes) => {
               {children}
             </div>
           )}
-          renderThumb={({ props, index }) => (
-            <div
-              {...props}
-              className='relative flex items-center justify-center'
-            >
+          renderThumb={({ props, index }) => {
+            const { key, ...restProps } = props;
+            return (
               <div
-                style={{
-                  position: 'absolute',
-                  top: '-21px',
-                  fontSize: '9px',
-                  background: '#206F6A',
-                  color: 'white',
-                  padding: '2px 6px',
-                  borderRadius: '4px',
-                  whiteSpace: 'nowrap',
-                }}
+                key={key}
+                {...restProps}
+                className='relative flex items-center justify-center'
               >
-                {values[index]} ha
-              </div>
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: '-21px',
+                    fontSize: '9px',
+                    background: '#206F6A',
+                    color: 'white',
+                    padding: '2px 6px',
+                    borderRadius: '4px',
+                    whiteSpace: 'nowrap',
+                  }}
+                >
+                  {values[index]} {unit}
+                </div>
 
-              <div
-                style={{
-                  height: '18px',
-                  width: '18px',
-                  borderRadius: '50%',
-                  backgroundColor: '#fff',
-                  border: '3px solid #7B9FF2',
-                  boxShadow: '0px 2px 6px rgba(0,0,0,0.2)',
-                }}
-              ></div>
-            </div>
-          )}
+                <div
+                  style={{
+                    height: '18px',
+                    width: '18px',
+                    borderRadius: '50%',
+                    backgroundColor: '#fff',
+                    border: '3px solid #7B9FF2',
+                    boxShadow: '0px 2px 6px rgba(0,0,0,0.2)',
+                  }}
+                ></div>
+              </div>
+            );
+          }}
         />
       </div>
-      <p className='mt-3 text-[10px] text-gray-500'>{details}</p>
+      {details && <p className='mt-3 text-[10px] text-gray-500'>{details}</p>}
     </div>
   );
 };
--- src/types/select-options.tsx
+++ src/types/select-options.tsx
@@ -2,11 +2,19 @@ export const sortOptions = [
   'Sortieren nach Eignung',
   'Sortieren nach Bundesland',
   'Sortieren nach Größe',
-  'Sortieren nach PLZ',
 ];
 
 export const profileOptions = [
-  "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
-]
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+];
 
 export const defaultOptions = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
