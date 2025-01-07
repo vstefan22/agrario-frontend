6cdf9e6ceafec20477ccdafc4ad64779c69346f8
aleksasavic1 <asavic846@gmail.com>
2025-01-07 02:04:01 +0100

    Fix Styling on Messages Page

--- src/components/landowner/messages/MessageItem.tsx
+++ src/components/landowner/messages/MessageItem.tsx
@@ -13,35 +13,37 @@ const MessageItem = ({ name, message, time, category }: MessageItemTypes) => {
   return (
     <div className='flex justify-between w-full'>
       <div className='flex'>
-        <div className='flex'>
-          <h2 className='font-bold'>{name}</h2>
+        <div className='flex items-center w-[248px]'>
+          <h2 className='font-bold whitespace-nowrap text-black-muted text-sm'>
+            {name}
+          </h2>
           {category === 'Work' && (
             <img
               src={workImg}
               alt='work icon'
-              className='object-contain ml-4'
+              className='object-contain ml-4 max-w-[60px] max-h-[22px]'
             />
           )}
           {category === 'Social' && (
             <img
               src={socialImg}
               alt='social icon'
-              className='object-contain ml-4'
+              className='object-contain ml-4 max-w-[60px] max-h-[22px]'
             />
           )}
           {category === 'Friends' && (
             <img
               src={friendsImg}
               alt='friends icon'
-              className='object-contain ml-4'
+              className='object-contain ml-4 max-w-[60px] max-h-[22px]'
             />
           )}
         </div>
       </div>
-      <div className='flex-1 ml-8'>
-        <p>{message}</p>
+      <div className='flex-1 pr-12'>
+        <p className='text-black-muted/90'>{message}</p>
       </div>
-      <span className='text-sm text-gray-500'>{time}</span>
+      <span className='text-sm text-black-muted/70'>{time}</span>
     </div>
   );
 };
