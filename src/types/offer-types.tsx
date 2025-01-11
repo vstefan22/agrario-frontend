5fd9d0eb70f8fee25e41f9735b34ea46154c0f4a
Mladen Todorovic <tm.djomla79@gmail.com>
2025-01-11 15:36:40 +0100

    Add actual offer preparation select options and fix field naming

--- src/types/offer-types.tsx
+++ src/types/offer-types.tsx
@@ -2,15 +2,16 @@ import { PlotType } from './plot-types';
 
 export type OfferType = {
   available_from: Date | null;
-  criteria1: string | null;
-  criteria2: string | null;
-  criteria3: string | null;
+  utilization: string | null;
+  preferred_regionality: string | null;
+  shareholder_model: string | null;
   no_usage_restriction: boolean;
   wind_energy_restriction: boolean;
   solar_energy_restriction: boolean;
   energy_storage_restriction: boolean;
   eco_enhancements_restriction: boolean;
-  message: string;
+  important_remarks: string;
+  hide_from_search: boolean;
   files: File[];
   is_owner_or_authorized: boolean;
   accept_privacy_policy: boolean;
